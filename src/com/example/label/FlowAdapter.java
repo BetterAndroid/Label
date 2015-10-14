6037525f94dc5bc65b1494fdcbe01df25027fd36
BetterAndroid <292449150@qq.com>
2015-10-14 15:28:10 +0800

    标签
    标签

---
+++ src/com/example/label/FlowAdapter.java
@@ -0,0 +1,51 @@
+package com.example.label;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * 老师标签的数据适配器a
+ * 说明：
+ * @author 51talk
+ * @version 创建时间：2015-10-14  下午2:28:23
+ */
+public class FlowAdapter {
+	
+	private Context mContext;
+	private ArrayList<String> data;
+	
+	public FlowAdapter(Context mContext, ArrayList<String> data) {
+		super();
+		this.mContext = mContext;
+		this.data = data;
+	}
+
+	public int getCount() {
+		return data == null ? 0 : data.size();
+	}
+	
+	public void setData(ArrayList<String> data) {
+		this.data = data;
+	}
+	
+	public View getView(FlowLayout parent, int position) {
+		// TODO Auto-generated method stub
+        Button tvNav = new Button(mContext);
+        tvNav.setBackgroundResource(R.drawable.teacher_label_selector);
+        tvNav.setTextSize(14);
+        tvNav.setPadding(20, 0, 20, 0);
+        // tvNav.setWidth((int)Math.floor(Double.valueOf(a)));
+        // tvNav.setHeight((int)Math.floor(Double.valueOf(b)));
+        tvNav.setText(data.get(position));
+        tvNav.setId(position);
+        tvNav.setTag(false);
+        tvNav.setTextColor(Color.BLACK);
+        // tvNav.setGravity(Gravity.CENTER_HORIZONTAL);
+        return tvNav;
+	}
+	
+}
---
+++ src/com/example/label/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.label;
+
+import java.util.ArrayList;
+
+import com.example.label.FlowLayout.OnItemClickListrener;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	private ArrayList<String> pingjia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        pingjia = new ArrayList<String>();
+        pingjia.add("welcome");
+        pingjia.add("IT工程师");
+        pingjia.add("挣钱ing");
+        pingjia.add("i think i can");
+        pingjia.add("标题栏textview");
+        pingjia.add("点击事件");
+        pingjia.add("显示到上面");
+        pingjia.add("清楚");
+        pingjia.add("明白易理解");
+//        pingjia.addAll(JsonUtils.getBeanList(message.result,"Items", Pingjia.class));
+        FlowLayout layout = (FlowLayout)findViewById(R.id.layou23);
+        FlowAdapter adapter = new FlowAdapter(this, pingjia);
+        layout.setAdapter(adapter);
+        layout.setOnItemClickListrener(new OnItemClickListrener() {
+			
+			@Override
+			public void onItemClick(ViewGroup parent, View v, int position) {
+				// TODO Auto-generated method stub
+//				Toast.makeText(getApplicationContext(), ((Button)v).getText().toString()+"------"+position, Toast.LENGTH_SHORT).show();
+//				((Button)v).setBackgroundColor(Color.GRAY);
+				
+				if(((Button)v).isSelected()) {
+					((Button)v).setSelected(false);
+					checkList.remove(pingjia.get(position));
+					checkSize --;
+				} else {
+					if(checkSize >= 5) {
+						Toast.makeText(getApplicationContext(), checkList.toString(), Toast.LENGTH_SHORT).show();
+//						Toast.makeText(getApplicationContext(), "最多选5个", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					((Button)v).setSelected(true);
+					checkList.add(pingjia.get(position));
+					checkSize++;
+				}
+				
+			}
+		});
+        
+    }
+
+    private ArrayList<String> checkList = new ArrayList<String>();
+    
+    private int checkSize = 0;
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
